084151e4ab0fa9ed4bd6f0eda822719f1ecf8176
kharisisriyant <kharisisriyant@gmail.com>
2017-10-27 14:44:09 +0700

    add login peserta

--- index.js
+++ index.js
@@ -234,6 +234,55 @@ app.get('/hasil/total', function(request, response) {
     }
 })
 
+
+// Hashing helper
+function sha256(input, secret) {
+  var crypto = require('crypto')
+  const hash = crypto.createHmac('sha256', secret)
+    .update(input)
+    .digest('hex');
+
+  return hash;
+}
+
+app.post('/masuk/peserta', function (request, response) {
+    var peserta = require('./model/peserta')
+
+    var body = request.body
+    console.log(body)
+
+    var password = sha256(body['password'], '12345')
+
+    peserta.model.where({nama_akun: body['nama_akun']}).fetch().then(function (model) {
+        if (model) {
+            if (model.get('password') == password) {
+                response.json({
+                    sukses: true,
+                    api_key: model.get('api_key'),
+                    pesan: "Berhasil"
+                })
+                return;                
+            } else {
+                response.json({
+                    sukses: false,
+                    pesan: "Password salah"
+                })
+                return;
+            }
+        } else {
+            response.json({
+                sukses: false,
+                pesan: "Nama akun tidak terdaftar"
+            })
+            return;
+        }
+    }).catch(function(err) {
+        console.log('Fetching failed')
+        console.log(err)
+        response.status(500).send("Cannot validate peserta: " + err)
+    })
+})
+
 app.get('/tpi/all', function(request, response) {
     var peserta = require('./model/peserta')
     var tpi = require('./model/tpi')
