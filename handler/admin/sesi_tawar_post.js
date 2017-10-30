e69fa28effc199e75a1002e02ef007f59b35c399
Harry Alvin Waidan Kefas <harry.kambey@gmail.com>
2017-10-30 12:23:14 +0700

    add boilerplate

---
+++ handler/admin/sesi_tawar_post.js
@@ -0,0 +1,47 @@
+module.exports = function (request, response) {
+
+  var sesi = require('../../model/sesi')
+  var admin = require('../../model/admin_tpi')
+  var hasil_ikan = require('../../model/hasil_ikan')
+  var ikan = require('../../model/ikan')
+  var penawaran = require('../../model/penawaran')
+
+  body = request.body
+  console.log(body)
+
+  if (!body['api_key']) {
+    response.json({
+      sukses: false,
+      pesan: "API Key tidak ada"
+    })
+    return;
+  }
+
+  admin.model.where({ api_key: body['api_key'] ? body['api_key'] : '' }).fetch().then(function (model) {
+    if (model) {
+      // API Key Checking
+      if (model.toJSON() == {}) {
+        response.json({
+          msg: "Invalid Credentials"
+        })
+      }
+      return;
+    } else {
+      response.json({
+        msg: "Something is wrong with your API Key. Your data is not here. Contact the administrator."
+      })
+      return;
+    }
+
+    var id = body['id_sesi']
+
+    response.send('Boilerplate')
+
+
+  })
+    .catch(function (error) {
+      console.log('Transaction failed')
+      console.log(error)
+      response.status(500).send("Error while retrieve admin: " + error)
+    })
+}
