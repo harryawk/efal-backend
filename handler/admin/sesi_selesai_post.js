3c0b55e7aa88075471b0672408963ad278f9d337
kharisisriyant <kharisisriyant@gmail.com>
2017-10-30 12:48:49 +0700

    add sesi selesai, not yet tested

--- handler/admin/sesi_selesai_post.js
+++ handler/admin/sesi_selesai_post.js
@@ -1,5 +1,16 @@
+function randomString() {
+	var text = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (var i = 0; i < 5; i++)
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+	return text;
+}
+
 module.exports = function(request, response) {
 	var sesi = require('../../model/sesi')
+	var penawaran = require('../../model/penawaran')
 	var moment = require('moment')
 
 	var body = request.body
@@ -17,12 +28,48 @@ module.exports = function(request, response) {
 		.save({
 			status: 3,
 			jam_selesai: moment().format('YYYY-MM-DD HH:mm:ss')
-		}, { patch: true }).then(function(model) {
+		}, { patch: true }).then(function(sesi_model) {
+			//iterasi semua penawaran, kasih kode kemenangan buat semua yg dapet 
+			penawaran.model.where({sesi_id: body['id_sesi']}).orderBy('harga_tawaran').fetchAll().then(function (model) {
+				var count = 0
+				model.count().then(function (count_result) {
+					count = count_result
+				})
+				sum_berat = 0
+				for (int i=0; i<count; i++) {
+					if (sum_berat < sesi_model.get('berat')) {
+						berat_dapat = 0
+						kode_kemenangan = randomString()
+						if (sum_berat + model[i].get('berat_kebutuhan') > sesi_model.get('berat')) {
+							berat_dapat = sesi_model.get('berat') - sum_berat
+							sum_berat = sesi_model.get('berat')
+						} else {
+							sum_berat += model[i].get('berat_kebutuhan')
+
+							berat_dapat = model[i].get('berat_kebutuhan')
+						}
+
+						new penawaran.model({id: model[i].get('berat_kebutuhan')}, {patch: true})
+							.save({
+								berat_dapat: berat_dapat
+								kode_kemenangan: kode_kemenangan
+							})
+							.then(function (model) {
+
+							})
+							.catch(function (error) {
+								console.log(error)
+							})
+					}
+				}
+			})
+
 			response.json({
-			sukses: true
-		})
-		return;
-	}) // add catch - update hasil_nelayan
+				sukses: true,
+				daftar_penawar: 
+			})
+			return;
+		}) // add catch - update hasil_nelayan
 
 
 	// sesi.model.where({id: body['id_sesi']}).fetch().then(function(model) {
