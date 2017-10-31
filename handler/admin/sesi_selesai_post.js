9555eb61541a486608d1dc1a3c79b48c8639450e
kharisisriyant <kharisisriyant@gmail.com>
2017-10-31 15:43:21 +0700

    finish sesi/selesai

--- handler/admin/sesi_selesai_post.js
+++ handler/admin/sesi_selesai_post.js
@@ -12,6 +12,7 @@ module.exports = function(request, response) {
 	var sesi = require('../../model/sesi')
 	var penawaran = require('../../model/penawaran')
 	var moment = require('moment')
+	var peserta = require('../../model/peserta')
 
 	var body = request.body
 	console.log(body)
@@ -23,107 +24,73 @@ module.exports = function(request, response) {
 		return
 	}
 
-
-    new sesi.model({ id: body['id_sesi'] })
+	new sesi.model({ id: body['id_sesi'] })
 		.save({
 			status: 3,
 			jam_selesai: moment().format('YYYY-MM-DD HH:mm:ss')
-		}, { patch: true }).then(function(sesi_model) {
-			//iterasi semua penawaran, kasih kode kemenangan buat semua yg dapet 
-			penawaran.model.where({sesi_id: body['id_sesi']}).orderBy('harga_tawaran').fetchAll().then(function (model) {
-				var count = 0
-				model.count().then(function (count_result) {
-					count = count_result
-				})
-				sum_berat = 0
-				for (int i=0; i<count; i++) {
-					if (sum_berat < sesi_model.get('berat')) {
-						berat_dapat = 0
-						kode_kemenangan = randomString()
-						if (sum_berat + model[i].get('berat_kebutuhan') > sesi_model.get('berat')) {
-							berat_dapat = sesi_model.get('berat') - sum_berat
-							sum_berat = sesi_model.get('berat')
-						} else {
-							sum_berat += model[i].get('berat_kebutuhan')
-
-							berat_dapat = model[i].get('berat_kebutuhan')
-						}
-
-						new penawaran.model({id: model[i].get('berat_kebutuhan')}, {patch: true})
-							.save({
-								berat_dapat: berat_dapat
-								kode_kemenangan: kode_kemenangan
-							})
-							.then(function (model) {
-
-							})
-							.catch(function (error) {
-								console.log(error)
-							})
-					}
-				}
-			})
-
-			response.json({
-				sukses: true,
-				daftar_penawar: 
-			})
-			return;
+		}, { patch: true })
+		.then(function(sesi_model) {
 		}) // add catch - update hasil_nelayan
+		.catch(function (error) {
+			console.log(error)
+		})
 
+	sesi.model.where({id: body['id_sesi']}).fetch().then(function (sesi_model) {
+		//iterasi semua penawaran, kasih kode kemenangan buat semua yg dapet
+		console.log('This is sesi_model')
+		console.log(sesi_model)
+		penawaran.model.where({sesi_id: body['id_sesi']})
+		.orderBy('harga_tawaran', 'DESC').fetchAll()
+		.then(function (model) {
+			console.log('This is sesi_model')
+			console.log(sesi_model.get('berat'))
+			var count = model.length
+			var sum_berat = 0
+			for (var i=0; i<count; i++) {
+				console.log(model.at(i))
+				if (sum_berat < sesi_model.get('berat')) {
+					console.log('masuk sini')
+					var berat_dapat = 0
+					var kode_kemenangan = randomString()
+					if (sum_berat + model.at(i).get('berat_kebutuhan') > sesi_model.get('berat')) {
+						berat_dapat = sesi_model.get('berat') - sum_berat
+						sum_berat = sesi_model.get('berat')
+					} else {
+						sum_berat += model.at(i).get('berat_kebutuhan')
+						berat_dapat = model.at(i).get('berat_kebutuhan')
+					}
 
-	// sesi.model.where({id: body['id_sesi']}).fetch().then(function(model) {
-	// 	if (model) {
-	// 		sesi.status = 2
-	// 		sesi.jam_mulai = Date.now()
-	// 		sesi.save().then(function (error) {
-	// 			response.json({
-	// 				sukses: true
-	// 			})
-	// 			return
-	// 		}).catch(function (error) {
-	// 			response.json({
-	// 				sukses: false,
-	// 				pesan: 'gagal save model'
-	// 			})
-	// 			return
-	// 		})
-	// 	} else {
-	// 		response.json({
-	// 			sukses: false,
-	// 			pesan: 'sesi tidak ditemukan'
-	// 		})
-	// 		return			
-	// 	}
-	// }).catch(function (error) {
-	// 	response.json({
-	// 		sukses: false,
-	// 		pesan: 'gagal retrieve sesi'
-	// 	})
-	// 	return		
-	// })
+					new penawaran.model({id: model.at(i).get('id')}, {patch: true})
+						.save({
+							berat_dapat: berat_dapat,
+							kode_kemenangan: kode_kemenangan
+						})
+						.then(function (model) {
 
-	// tpiadmin.model.where({api_key: body['api_key']}).fetch().then(function(model) {
-	// 	if (model) {
-	// 		sesi.model.where({tpi_id: model.get('tpi_id'), tanggal_sesi: body['tanggal']}).fetchAll().then(function(sesi_model) {
-	// 			response.json({
-	// 				daftar_sesi: sesi_model
-	// 			})
-	// 			return
-	// 		}).catch(function(error){
-	// 			console.log('Fetching failed')
-	// 			console.log(error)
-	// 			response.status(500).send("Cannot fetch admin: " + error)
-	// 		})
-	// 	} else {
- //          response.json({
- //            msg: "Invalid Credentials"
- //          })
- //          return;
-	// 	}
-	// }).catch(function(error){
- //        console.log('Fetching failed')
- //        console.log(error)
- //        response.status(500).send("Cannot fetch admin: " + error)
-	// })
+						})
+						.catch(function (error) {
+							console.log(error)
+						})
+				}
+			}
+
+			penawaran.model.where({sesi_id: body['id_sesi']})
+				.where('kode_kemenangan', '<>', 'NULL')
+				.fetchAll({withRelated: ['peserta']})
+				.then(function(penawaran_model) {
+					response.json({
+						sukses: true,
+						daftar_penawar: penawaran_model
+					})
+					return;
+				})
+				.catch(function (error) {
+					console.log(error)
+				})
+
+		})
+		.catch(function (error) {
+			console.log(error)
+		})
+	})
 }
