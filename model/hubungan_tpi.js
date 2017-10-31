726fb75f230e5306fb6c10cc2742aae12ba7cf96
kharisisriyant <kharisisriyant@gmail.com>
2017-10-31 20:47:00 +0700

    add /tpi get

--- model/hubungan_tpi.js
+++ model/hubungan_tpi.js
@@ -1,7 +1,10 @@
 var helper = require('./database.js');
 
 Model = helper.bookshelf.Model.extend({
-  tableName: 'hubungan_tpi'
+  tableName: 'hubungan_tpi',
+  tpi_terhubung: function() {
+  	return this.belongsTo('TPI', 'tpi_terhubung_id')
+  }
 });
 model = helper.bookshelf.model('HubunganTPI', Model);
 
